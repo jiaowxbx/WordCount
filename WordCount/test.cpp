cb470b4f02085967b38ac274816db888bb9856ae
jiaowxbx <913665719@qq.com>
2022-05-22 15:33:31 +0800

    更新

--- WordCount/test.cpp
+++ WordCount/test.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 	FILE *fp;
 	int count1=0;
 	int count2=0;
+	int flag=0,flagD=0; 
 	if (argc == 2)
 	{
 		printf("没有输入参数，当前文件: %s \n", argv[0]);
@@ -38,15 +39,30 @@ int main(int argc, char *argv[])
         fscanf(fp,"%c",&temp[j]);
         j++;
     }
-	for(int m=0;m<j-1;m++)
+    int m=0;
+	for(m=0;m<j-1;m++)
 	{
-		if(temp[m]>='a'&&temp[m]<='z')
+		if((temp[m]>='a'&&temp[m]<='z')||(temp[m]>='A'&&temp[m]<='Z'))
 		{
-			count1++;
+			flagD=1;
 		}
 		else{
 			count2++;
+			if(flagD==1)
+			{
+				flag=1;
+			}
 		}
+		if(flag!=0&&flagD!=0)
+		{
+			count1++;
+			flag=0;
+			flagD=0;
+		}
+	}
+	if((temp[m-1]>='a'&&temp[m-1]<='z')||(temp[m-1]>='A'&&temp[m-1]<='Z'))
+	{
+		count1++;
 	}
 	while((i = getopt(argc, argv, ":cw")) != -1)
    	switch(i)
